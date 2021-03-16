6b89b39e67a0a113d7735fefb4bb8d908b0394d1
rilesnyou <riley.newton93@yahoo.com>
2021-03-16 11:48:06 -0400

    working job link

--- src/main/java/scrap/app.java
+++ src/main/java/scrap/app.java
@@ -14,12 +14,14 @@ public class app {
 		try {
 		Document doc = Jsoup.connect("https://www.indeed.com/jobs?q=developer&l=Detroit%2C+MI").userAgent("mozilla/17.0").get();
 		Elements temp = doc.select("h2.title");
+		Elements link = doc.select("h2.title > a");
+		String url = link.attr("href");
 		int i=0;
 		for(Element resultsBody:temp)
 		{
 			i++;
 			System.out.println(i+" "+resultsBody.getElementsByTag("a").first().text());
-			System.out.println(i+" "+resultsBody.getElementById("h"));
+			System.out.println("https://www.indeed.com"+url);
 			// Elements s = resultsBody.getElementsByTag("a");
 			// System.out.println(s);
 		}
